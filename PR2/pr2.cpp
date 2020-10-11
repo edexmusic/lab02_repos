9fe03ad769cdf7393d3ba80571cbd621def55258
edexmusic <denys220403@gmail.com>
2020-10-11 12:18:54 +0300

    first master commit

---
+++ PR2/pr2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; 
+	double y; 
+	double z1;
+	double z2; 
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	z1 = pow(cos (x), 4) + pow(sin (y), 2) + 1.0 / 4 * pow(sin(2*x), 2) - 1;
+	z2 = (sin(y + x)) * (sin(y - x));
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
